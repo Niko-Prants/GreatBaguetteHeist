f07ab34b5c915d6cb723fac6119276da6e2e8053
niko <112471231+Niko-Prants@users.noreply.github.com>
2022-10-14 23:59:37 +0300

    enemy guard attempt

---
+++ Heist game/Assets/Scripts/EnemyGuard.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGuard : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int speed;
+
+    private int waypointIndex;
+    private float dis;
+
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dis = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+        if (dis < 1f)
+        {
+            IncreaseIndex();
+
+        }
+        Patrol();
+    }
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+}
